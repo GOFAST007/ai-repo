8b7fccc55380ea22bcb35a46e9a96a30838a5339
Electro <145841131+ayatweb@users.noreply.github.com>
2023-12-31 20:26:00 +0100

    Create create chatbot.py
    Signed-off-by: Electro <145841131+ayatweb@users.noreply.github.com>

---
+++ create chatbot.py
@@ -0,0 +1,41 @@
+create ai chatbot repo from cmd 
+
+
+To create an AI chatbot repository from the command line, you can follow these steps:
+
+Open the command prompt or terminal on your computer.
+
+Navigate to the directory where you want to create the repository.
+
+Use git init command to initialize a new repository:
+
+Copier le code
+
+git init chatbot-repo
+This will create a new directory named "chatbot-repo" and initialize it as a Git repository.
+
+Change your current working directory to the newly created repository:
+
+Copier le code
+
+cd chatbot-repo
+Create the necessary files for your chatbot. You can use a programming language of your choice, such as Python, JavaScript, or Java, depending on your requirements.
+
+To add the files to the repository, use the following command:
+
+Copier le code
+
+git add .
+This will add all files in the current directory to the Git repository.
+
+Commit the changes you made to the repository:
+
+Copier le code
+
+git commit -m "Initial commit"
+Replace "Initial commit" with an appropriate commit message that describes the initial state of your chatbot repository.
+
+Optionally, you can connect your local repository to a remote repository on a version control hosting platform like GitHub or GitLab. Follow the instructions provided by the platform to set up the remote repository and link it to your local repository.
+
+With these steps, you have created an AI chatbot repository using the command line. You can now continue to develop your chatbot and make further commits as needed.
+
